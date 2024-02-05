e8e095bcf81b029e94274de15486aebc73b37b7d
duzhg56980 <guokey1996@gmail.com>
2024-02-05 22:31:01 +0800

    init commit

---
+++ 473160-0.8.1/FunctionUse863.java
@@ -0,0 +1,7 @@
+import ch.qos.logback.core.util.PropertySetter;
+public class FunctionUse863 {
+public void funcUse() {
+PropertySetter propertysetter = new PropertySetter();
+propertysetter.setProperty(null,null);
+}
+}
---
+++ 473160-0.8.1/FunctionUse877.java
@@ -0,0 +1,6 @@
+import ch.qos.logback.core.util.PropertySetter;
+public class FunctionUse877 {
+public void funcUse() {
+PropertySetter propertysetter = new PropertySetter();
+}
+}
